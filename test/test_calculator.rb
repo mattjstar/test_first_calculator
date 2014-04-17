6589cf0e5e9f6d39074ac45a1fb873e3c95bd98a
Matthew Star <mattjstar@gmail.com>
2014-04-17 17:11:58 -0400

    add multiply method

--- test/test_calculator.rb
+++ test/test_calculator.rb
@@ -2,16 +2,22 @@ require 'test/unit'
 require 'app/calculator'
 
 class CalculatorTest < Test::Unit::TestCase
+  def setup
+    @calculator = Calculator.new
+  end
+	
   def test_can_add
-    calculator = Calculator.new
-    actual = calculator.add(2, 2)
+    actual = @calculator.add(2, 2)
     assert_equal(4, actual)
   end
 
   def test_can_subtract
-    calculator = Calculator.new
-    actual = calculator.subtract(5, 3)
+    actual = @calculator.subtract(5, 3)
     assert_equal(2, actual)
   end
 
+  def test_can_multiply
+    actual = @calculator.multiply(2, 10)
+    assert_equal(20, actual)
+  end
 end
